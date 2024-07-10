21fc24d1264362edd07bc0283ee181c5d79ad2d6
dlopatin <dany.0905200571@gmail.com>
2024-07-10 13:43:31 +0300

    пустой плагин

--- src/ru/charliesadizt/firstplugin/Main.java
+++ src/ru/charliesadizt/firstplugin/Main.java
@@ -1,11 +1,24 @@
 package ru.charliesadizt.firstplugin;
 
+//import org.bukkit.entity.Player;
+//import org.bukkit.event.block.BlockBreakEvent;
+//import org.bukkit.event.player.PlayerJoinEvent;
+//import org.bukkit.inventory.Recipe;
 import org.bukkit.plugin.java.JavaPlugin;
+
+//import net.minecraft.server.v1_16_R3.PlayerAbilities;
 
 public class Main extends JavaPlugin {
     @Override
     public void onEnable() {
         getLogger().info("PLUGIN IS WORKING NOW!!!!");
-        //super.onEnable();
+        //getServer().
+    }
+
+    private void toDoSomething(){
+
+        //getServer().
+        //Player asd = new Player();
+        //getServer().getOnlinePlayers().forEach(Player);
     }
 }

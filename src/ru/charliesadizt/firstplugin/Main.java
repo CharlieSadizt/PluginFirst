487ade960c714b16b6288af81cacf24aa0cc3471
dlopatin <dany.0905200571@gmail.com>
2024-07-10 11:18:48 +0300

    onEnable();

--- src/ru/charliesadizt/firstplugin/Main.java
+++ src/ru/charliesadizt/firstplugin/Main.java
@@ -3,5 +3,9 @@ package ru.charliesadizt.firstplugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin {
-    
+    @Override
+    public void onEnable() {
+        getLogger().info("PLUGIN IS WORKING NOW!!!!");
+        //super.onEnable();
+    }
 }
